0796395e2d8e606391f238d818d7005480f9bda2
Riazul Islam <riazul5207@gmail.com>
2024-06-23 20:56:14 +0600

    color palette updated

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -1,30 +1,9 @@
 import { PaletteColorOptions, PaletteOptions } from '@mui/material/styles';
-import {
-  grey,
-  red,
-  green,
-  blue,
-  cyan,
-  purple,
-  violate,
-  yellow,
-  white,
-  transparentRed,
-  transparentGreen,
-  transparentYellow,
-} from './colors';
+import { gray, darkGray, red, green, blue, yellow, white } from './colors';
 
 declare module '@mui/material/styles' {
   interface PaletteOptions {
     neutral?: PaletteColorOptions;
-    transparent?: {
-      success: PaletteColorOptions;
-      warning: PaletteColorOptions;
-      error: PaletteColorOptions;
-    };
-    gradients?: {
-      primary: PaletteColorOptions;
-    };
   }
   interface SimplePaletteColorOptions {
     lighter?: string;
@@ -33,14 +12,6 @@ declare module '@mui/material/styles' {
   }
   interface Palette {
     neutral: PaletteColor;
-    gradients: {
-      primary: PaletteColor;
-    };
-    transparent: {
-      success: PaletteColor;
-      warning: PaletteColor;
-      error: PaletteColor;
-    };
   }
   interface PaletteColor {
     lighter: string;
@@ -51,27 +22,19 @@ declare module '@mui/material/styles' {
 
 const palette: PaletteOptions = {
   neutral: {
-    lighter: grey[100],
-    light: grey[200],
-    main: grey[300],
-    dark: grey[400],
-    darker: grey[600],
+    main: gray[500],
   },
   primary: {
-    main: purple[500],
-    dark: purple[800],
+    main: blue[500],
+    dark: darkGray[500],
   },
   secondary: {
-    lighter: blue[200],
-    light: cyan[400],
-    main: cyan[500],
-    dark: cyan[900],
-    darker: blue[500],
+    main: blue[200],
   },
   info: {
-    main: blue[700],
-    dark: blue[800],
-    darker: blue[900],
+    light: white[100],
+    main: white[200],
+    dark: white[300],
   },
   success: {
     main: green[500],
@@ -83,26 +46,9 @@ const palette: PaletteOptions = {
     main: red[500],
   },
   text: {
-    primary: white[500],
-    secondary: grey[300],
-    disabled: grey[500],
-  },
-  gradients: {
-    primary: {
-      main: purple[500],
-      state: violate[600],
-    },
-  },
-  transparent: {
-    success: {
-      main: transparentGreen[500],
-    },
-    warning: {
-      main: transparentYellow[500],
-    },
-    error: {
-      main: transparentRed[500],
-    },
+    primary: darkGray[500],
+    secondary: gray[400],
+    disabled: gray[300],
   },
 };
 
