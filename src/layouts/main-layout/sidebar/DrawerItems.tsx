b4d768753f13f3b335150970e01d0aaf9a38af32
Riazul Islam <riazul5207@gmail.com>
2024-06-24 18:02:43 +0600

    sidebar section updated

--- src/layouts/main-layout/sidebar/DrawerItems.tsx
+++ src/layouts/main-layout/sidebar/DrawerItems.tsx
@@ -17,7 +17,8 @@ const DrawerItems = () => {
       <Stack
         position="sticky"
         top={0}
-        py={4}
+        pt={4}
+        pb={2.5}
         alignItems="center"
         bgcolor="info.light"
         zIndex={1000}
@@ -30,7 +31,7 @@ const DrawerItems = () => {
         </ButtonBase>
       </Stack>
 
-      <List component="nav">
+      <List component="nav" sx={{ mt: 3, mb: 15 }}>
         {sitemap.map((route) =>
           route.items ? (
             <CollapseListItem key={route.id} {...route} />
@@ -40,22 +41,29 @@ const DrawerItems = () => {
         )}
       </List>
 
-      <Stack position="relative" mt={10} mb={4} height={330} width={1} sx={{ userSelect: 'none' }}>
+      <Stack
+        position="relative"
+        mt="auto"
+        mb={4}
+        height={300}
+        width={1}
+        sx={{ userSelect: 'none' }}
+      >
         <Image src={FooterImg} height={1} width={1} sx={{ objectFit: 'cover' }} />
 
         <Stack px={1.75} direction="column" position="absolute" top={110} left={0} width={1}>
-          <Typography variant="body1" fontWeight="600" color="info.light" align="center" width={1}>
+          <Typography variant="body1" color="info.light" align="center" fontWeight={600} width={1}>
             Help Center
           </Typography>
           <Typography
             mt={1}
             variant="caption"
-            fontWeight={400}
             color="info.light"
             align="center"
+            fontWeight={400}
             width={1}
           >
-            Having Trouble in Learning? <br /> Please contact us for more questions.
+            Having Trouble in Learning? Please contact us for more questions.
           </Typography>
         </Stack>
 
