f175607cbcd48fa628a00203c704c1cb4ef7df2e
Riazul Islam <riazul5207@gmail.com>
2024-07-06 16:02:55 +0600

    code organized

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import { theme } from 'theme/theme.ts';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
